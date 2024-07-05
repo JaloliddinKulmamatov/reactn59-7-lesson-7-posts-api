9e511e46c26d592c15820618df6e72a4a7d14c14
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-07-05 21:45:27 +0500

    api

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 const path = require("path");
 
-const server = jsonServer. create();
-const router = jsonServer. router(path-join(_dirname, "db.json")) ;
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
+
 server.use(cors());
 server.use(jsonServer.bodyParser);
-server.use (middlewares);
-server. use (router);
+server.use(middlewares);
+server.use(router);
+
 const PORT = process.env.PORT || 8080;
 
-server. listen (PORT, () => {
-    console. log(`JSON Server is running on http://localhost:${PORT}`)
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
